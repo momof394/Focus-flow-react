340d0e7ce6e02254d5927a5c0e4febe62e4db669
Megan Card <megandcard94@gmail.com>
2025-07-08 16:43:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
+  root: 'src', // file:///C:/Users/Torma/OneDrive/Desktop/New%20folder/Focus-flow-react/public/index.html
 });
