cd2b11afedf894909ee4fe46ce1ddded31ee2db6
Megan Card <megandcard94@gmail.com>
2025-07-08 17:14:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src', // C:\Users\Torma\OneDrive\Desktop\New folder\Focus-flow-react\index.html
-  build: {
-    rollupOptions: {
-      input: 'relative/path/to/index.html'
-    }
-  }
 });
