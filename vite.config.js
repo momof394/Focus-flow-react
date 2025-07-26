501d382d13378f6fc83faca65df24411fc797cc3
Megan Card <megandcard94@gmail.com>
2025-07-25 22:45:15 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // This tells Vite to use the root of the project
-  build: {
-    outDir: 'dist',
-  },
+  root: 'public'
 })
